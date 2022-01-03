7c5a194230affc3a6ba30978bb253b8ad46b3095
szysz.bartlomiej <szysz.bartlomiej@gmail.com>
2022-01-03 08:07:46 +0100

    Code base for rainly_be

---
+++ measurement/MeasurementProcessor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from measurement.MeasurementReader import MeasurementReader
+from measurement.MeasurementSender import MeasurementSender
+
+
+class MeasurementProcessor(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        measurementReader = MeasurementReader(interval=300)
+        measurementSender = MeasurementSender(destination_url=r'http://localhost:5000/add_measurement')
+
+        while True:
+            year, month, day, clock_time, rainfall = measurementReader.read()
+            measurementSender.send(year=year,
+                                   month=month,
+                                   day=day,
+                                   clock_time=clock_time,
+                                   rainfall=rainfall)
+
+if __name__ == "__main__":
+    measurementProcessor = MeasurementProcessor()
+    measurementProcessor.run()
+    print("Measurement processor executed successfully!")
