1a5be34a28e9da3b09d67c11af4f4e44e328b5b4
Bartlomiej Szysz <szysz.bartlomiej@gmail.com>
2022-04-08 18:32:05 +0200

    Fix typo

--- measurement/MeasurementReader.py
+++ measurement/MeasurementReader.py
@@ -8,7 +8,7 @@ def wait_for(interval):
     start_time = time.time()
     while time.time() - start_time <= interval:
         print("Measurement in process...")
-        time.sleep(60)
+        time.sleep(10)
 
 class MeasurementReader(object):
     BUCKET_SIZE = 0.2794
@@ -30,7 +30,7 @@ class MeasurementReader(object):
         rainfall = round(self.tip_count * self.BUCKET_SIZE, 2)
         timestamp = datetime.datetime.now()
         self.reset_rainfall()
-        print(f"Read rainfall: {rainfall} mm t timestamp {timestamp.strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"Read rainfall: {rainfall} mm at timestamp {timestamp.strftime('%Y-%m-%d %H:%M:%S')}")
         return timestamp, rainfall
 
 if __name__ == "__main__":
